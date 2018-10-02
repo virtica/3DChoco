6a0fdd8170dcf299e1477690eee76dcae7838de1
Kenneth Watkins <kenneth@blue1647.com>
2018-10-02 12:36:01 -0500

    initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/about', (req,res) => res.send("This is the about section"))
+app.get('/page1', (req,res) => res.send("Welcome to page 1"))
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
