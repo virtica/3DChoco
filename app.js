cde6a994be1e2fe54c3026fe6d65e8099cdc8ae8
Kenneth Watkins <kenneth@blue1647.com>
2018-10-03 11:33:48 -0500

    Created footer, header, layout, style and navigation templates

--- app.js
+++ app.js
@@ -9,11 +9,7 @@ app.set('views' , './views')
 app.set('view engine', 'pug')
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/about', (req,res) => res.send("This is the about section"))
-app.get('/page1', (req,res) => res.send("Welcome to page 1"))
-app.get('/home', (req, res) =>
-res.render('home',{title: "Home", message:"Welcome to the home page"}))
+app.get('/', (req, res) => res.render('home'), {title: "Blue|1647 Chocolate 3d Printer"})
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
